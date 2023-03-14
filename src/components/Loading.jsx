c6f31e04a1af847061d11297b32c0f2d0b92575c
Krzysztof Sikora <krzysieksikora00123@gmail.com>
2023-03-14 14:28:37 +0100

    fixed loading

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,10 +1,10 @@
 import React from "react";
-import { ScaleLoader } from "react-spinners";
+import { ClipLoader } from "react-spinners";
 
 const Loading = () => {
   return (
-    <div className=" h-screen w-full  absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2  flex items-center justify-center z-40">
-      <ScaleLoader color="#10b981" />
+    <div className="  w-full h-full absolute top-0 left-0   flex items-center justify-center z-40">
+      <ClipLoader color="#10b981" />
     </div>
   );
 };
