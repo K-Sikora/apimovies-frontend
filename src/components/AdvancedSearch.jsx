0f5602a23804a98d1be70358b970d3b2a22c14ff
Krzysztof Sikora <krzysieksikora00123@gmail.com>
2023-03-30 14:55:26 +0200

    all good

--- src/components/AdvancedSearch.jsx
+++ src/components/AdvancedSearch.jsx
@@ -8,19 +8,12 @@ import axios from "axios";
 import NotFound from "./NotFound";
 import { motion, AnimatePresence } from "framer-motion";
 const AdvancedSearch = () => {
-  const [sortMovie, setSortMovie] = useState("popularity.desc");
-
-  useEffect(() => {
-    console.log(sortMovie);
-    refetch();
-  }, [sortMovie]);
   const [movieCategoriesVisible, setMovieCategoriesVisible] = useState(true);
   const [TvCategoriesVisible, setTvCategoriesVisible] = useState(false);
   const getGenres = async () => {
     const response = await axios.get(
       "https://apimovies-backend.onrender.com/api/moviegenres"
     );
-    console.log(response.data);
     return response.data;
   };
   const { data: movieGenres, isLoading } = useQuery({
@@ -44,6 +37,8 @@ const AdvancedSearch = () => {
 
   const [currentSelect, setCurrentSelect] = useState();
   const handleTypeSelect = (e) => {
+    setChosenGenres([]);
+    setChosenGenresTv([]);
     setCurrentSelect(e.target.value);
     if (e.target.value === "Movie") {
       setMovieCategoriesVisible(true);
@@ -59,11 +54,48 @@ const AdvancedSearch = () => {
     }
   }, []);
   const [chosenGenres, setChosenGenres] = useState([]);
-  const [movieYear, setMovieYear] = useState(2015);
+  const [movieYear, setMovieYear] = useState();
+  const [sortMovie, setSortMovie] = useState("popularity.desc");
+  const [chosenGenresTv, setChosenGenresTv] = useState([]);
+  const [tvYear, setTvYear] = useState();
+  const [sortTv, setSortTv] = useState("popularity.desc");
+  useEffect(() => {
+    refetch();
+  }, [sortMovie]);
+  useEffect(() => {
+    if (currentSelect === "Movie") {
+      refetch();
+    } else {
+      refetch2();
+    }
+  }, [movieYear]);
+  //movie call
   const getMovieResults = async () => {
     const response = await axios.get(
       `https://apimovies-backend.onrender.com/api/movie-advanced/${movieYear}/${sortMovie}/${
-        chosenGenres.length === 0 ? 14 : chosenGenres.toString()
+        chosenGenres.length === 0 ? "b" : chosenGenres.toString()
+      }`
+    );
+    return response.data;
+  };
+
+  const {
+    data: movieResults,
+    refetch: refetch,
+    isRefetching: isRefetching,
+    isLoading: isLoading2,
+  } = useQuery({
+    queryKey: ["movieResults", sortMovie, movieYear],
+    queryFn: getMovieResults,
+    refetchOnWindowFocus: false,
+  });
+  //end movie call
+
+  //tv call
+  const getTvResults = async () => {
+    const response = await axios.get(
+      `https://apimovies-backend.onrender.com/api/tv-advanced/${movieYear}/${sortMovie}/${
+        chosenGenresTv.length === 0 ? "b" : chosenGenresTv.toString()
       }`
     );
     console.log(response.data);
@@ -71,16 +103,16 @@ const AdvancedSearch = () => {
   };
 
   const {
-    data: movieResults,
-    refetch,
-    isRefetching,
-    isLoading: isLoading2,
+    data: tvResults,
+    refetch: refetch2,
+    isRefetching: isRefetching2,
+    isLoading: isLoading3,
   } = useQuery({
-    queryKey: ["movieResults"],
-    queryFn: getMovieResults,
+    queryKey: ["tvResults", sortMovie, movieYear],
+    queryFn: getTvResults,
     refetchOnWindowFocus: false,
   });
-
+  //end tv call
   if (isLoading) {
     return <NotFound />;
   }
@@ -134,7 +166,16 @@ const AdvancedSearch = () => {
             <div className="mb-2">
               <input
                 onChange={(e) => {
-                  setMovieYear(e.target.value);
+                  if (
+                    e.target.value.length === 4 &&
+                    parseInt(e.target.value) > 1895 &&
+                    parseInt(e.target.value) <= 2027
+                  ) {
+                    setMovieYear(e.target.value);
+                  }
+                  if (e.target.value.length === 0) {
+                    setMovieYear("b");
+                  }
                 }}
                 className="w-40 bg-dark-900/5 px-2 py-1 focus:shadow-lg focus:shadow-dark-900/5 -outline-offset-2 outline-none outline-emerald-500 focus:outline-2 focus:outline-emerald-600 rounded-md duration-300"
                 type="text"
@@ -176,7 +217,6 @@ const AdvancedSearch = () => {
                               onChange={(e) => {
                                 if (e.target.checked === true) {
                                   chosenGenres.push(e.target.dataset.id);
-                                  console.log(chosenGenres);
                                 } else {
                                   chosenGenres.indexOf(e.target.dataset.id) !==
                                     -1 &&
@@ -184,7 +224,6 @@ const AdvancedSearch = () => {
                                       chosenGenres.indexOf(e.target.dataset.id),
                                       1
                                     );
-                                  console.log(chosenGenres);
                                 }
                                 refetch();
                               }}
@@ -223,6 +262,24 @@ const AdvancedSearch = () => {
                             className="flex justify-center items-center pb-1 border-b-2 border-emerald-500"
                           >
                             <input
+                              onChange={(e) => {
+                                if (e.target.checked === true) {
+                                  chosenGenresTv.push(e.target.dataset.id);
+                                } else {
+                                  chosenGenresTv.indexOf(
+                                    e.target.dataset.id
+                                  ) !== -1 &&
+                                    chosenGenresTv.splice(
+                                      chosenGenresTv.indexOf(
+                                        e.target.dataset.id
+                                      ),
+                                      1
+                                    );
+                                }
+                                console.log(chosenGenresTv);
+                                refetch2();
+                              }}
+                              data-id={item.id}
                               type="checkbox"
                               name={item.name}
                               id={item.name}
@@ -246,13 +303,12 @@ const AdvancedSearch = () => {
         <div></div>
       </div>
       <section className=" max-w-6xl mx-auto min-h-screen">
-        {isRefetching || isLoading2 ? (
+        {isRefetching || isLoading2 || isRefetching2 ? (
           <div className=" translate-y-20">
             <Loading />
           </div>
-        ) : (
-          movieResults &&
-          movieResults.results.map((item) => (
+        ) : movieCategoriesVisible ? (
+          movieResults?.results.map((item) => (
             <motion.div
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
@@ -288,19 +344,82 @@ const AdvancedSearch = () => {
                     textColor: "white",
                     trailColor: "#065f46",
                     pathColor: `
-          ${
-            item.vote_average * 10 <= 30
-              ? `#ef4444`
-              : item.vote_average * 10 > 30 && item.vote_average * 10 <= 50
-              ? `#f97316`
-              : item.vote_average * 10 > 50 && item.vote_average * 10 <= 70
-              ? `#facc15`
-              : item.vote_average * 10 > 70 && item.vote_average * 10 <= 84
-              ? `#059669`
-              : `#10b981`
-          }
-          
-          `,
+              ${
+                item.vote_average * 10 <= 30
+                  ? `#ef4444`
+                  : item.vote_average * 10 > 30 && item.vote_average * 10 <= 50
+                  ? `#f97316`
+                  : item.vote_average * 10 > 50 && item.vote_average * 10 <= 70
+                  ? `#facc15`
+                  : item.vote_average * 10 > 70 && item.vote_average * 10 <= 84
+                  ? `#059669`
+                  : `#10b981`
+              }
+              
+              `,
+                  })}
+                  className="h-10 w-10 "
+                  value={item.vote_average * 10}
+                  text={`${item.vote_average.toFixed(1) * 10 + "%"}`}
+                ></CircularProgressbar>
+              </div>
+            </motion.div>
+          ))
+        ) : (
+          tvResults?.results.map((item) => (
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ duration: 1.5 }}
+              className="flex h-40 relative transition-colors duration-300 gap-4 bg-light dark:bg-stone-900  shadow-xl dark:shadow-stone-900/60 shadow-stone-900/30 mx-6 mt-6 pr-2 rounded-md"
+            >
+              <div className=" h-32 flex-shrink-0  ">
+                <Link to={`/movie/${item.id}`}>
+                  <img
+                    className=" h-40 w-28 object-cover rounded-l-md pointer-events-none "
+                    src={`https://image.tmdb.org/t/p/w300` + item.poster_path}
+                  />
+                </Link>
+              </div>
+              <div className="flex   justify-around flex-col h-full">
+                <div>
+                  <Link to={`/movie/${item.id}`}>
+                    <h4 className="text-base dark:text-light text-dark-900 md:text-xl inline-block">
+                      {item.name}
+                    </h4>
+                  </Link>
+
+                  <p className="text-sm text-stone-400">
+                    {item.first_air_date}
+                  </p>
+                </div>
+                <p className="line-clamp dark:text-light text-dark-700 text-sm md:text-base">
+                  {item.overview}{" "}
+                </p>
+              </div>
+              <div className="absolute left-1 bg-stone-900/80 rounded-full bottom-2">
+                <CircularProgressbar
+                  styles={buildStyles({
+                    textSize: "28px",
+                    textColor: "white",
+                    trailColor: "#065f46",
+                    pathColor: `
+                ${
+                  item.vote_average * 10 <= 30
+                    ? `#ef4444`
+                    : item.vote_average * 10 > 30 &&
+                      item.vote_average * 10 <= 50
+                    ? `#f97316`
+                    : item.vote_average * 10 > 50 &&
+                      item.vote_average * 10 <= 70
+                    ? `#facc15`
+                    : item.vote_average * 10 > 70 &&
+                      item.vote_average * 10 <= 84
+                    ? `#059669`
+                    : `#10b981`
+                }
+                
+                `,
                   })}
                   className="h-10 w-10 "
                   value={item.vote_average * 10}
